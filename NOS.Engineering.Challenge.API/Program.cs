c2e780500ead49a2a64d465645d82985905ffb8e
Ruben Cipriano <rubencipriano13@gmail.com>
2024-05-03 14:12:13 +0100

    Removing one Using

--- NOS.Engineering.Challenge.API/Program.cs
+++ NOS.Engineering.Challenge.API/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using NOS.Engineering.Challenge.API.Extensions;
-using NOS.Engineering.Challenge.ApplicationDBContext;
 
 var builder = WebApplication.CreateBuilder(args)
         .ConfigureWebHost()
