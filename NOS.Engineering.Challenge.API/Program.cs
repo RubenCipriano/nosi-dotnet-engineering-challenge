c9e7081d889978d632eae8d5cd572e6c688d3729
Ruben Cipriano <rubencipriano13@gmail.com>
2024-05-03 12:25:57 +0100

    Task 3

--- NOS.Engineering.Challenge.API/Program.cs
+++ NOS.Engineering.Challenge.API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using NOS.Engineering.Challenge.API.Extensions;
+using NOS.Engineering.Challenge.ApplicationDBContext;
 
 var builder = WebApplication.CreateBuilder(args)
         .ConfigureWebHost()
         .RegisterServices();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString, b => b.MigrationsAssembly("NOS.Engineering.Challenge.API")));
+
 var app = builder.Build();
 
 app.MapControllers();
